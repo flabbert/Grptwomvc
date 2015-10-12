e9a5c7231b38b1eb9bfcc3ba31e44631e099066e
Gustav Marius Karto <flabbert@outlook.com>
2015-10-12 10:56:57 +0200

    initial commit

---
+++ Grptwomvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Grptwomvc.Startup))]
+namespace Grptwomvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
