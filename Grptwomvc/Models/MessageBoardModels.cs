4f2b138769fdc7381ca405461d88e0050b49eb32
Gustav Marius Karto <flabbert@outlook.com>
2015-10-19 10:13:42 +0200

    databaseskit

---
+++ Grptwomvc/Models/MessageBoardModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grptwomvc.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+    }
+    public class Post
+    {
+        public int Id { get; set; }
+        public int MessageId { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+
+    }
+    
+}
