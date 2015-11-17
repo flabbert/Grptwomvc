5b87902c4df671e4246186800f89dfed1e707c96
Gustav Marius Karto <flabbert@outlook.com>
2015-11-17 09:22:07 +0100

    lägga til grptwo.js och lägga den til script bundles

--- Grptwomvc/App_Start/BundleConfig.cs
+++ Grptwomvc/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace Grptwomvc
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/grptwo.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
