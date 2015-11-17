a86415849c3d92fc4b3faefd42e30633e79cd8cb
Gustav Marius Karto <flabbert@outlook.com>
2015-11-17 11:18:32 +0100

    jquery data validation

--- Grptwomvc/Scripts/grptwo.js
+++ Grptwomvc/Scripts/grptwo.js
@@ -1,18 +1,39 @@
 ﻿$(function () {
     var ajaxFormSubmit = function () {
         var $form = $(this);
-
+        var error_free = true;
+        var error_message = "";
         var options = {
             url: $form.attr("action"),
             type: $form.attr("method"),
             data: $form.serialize()
         };
-        $.ajax(options).done(function (data) {
-            var $target = $($form.attr("data-gt-target"));
-            $target.replaceWith(data);
-        });
-        return false;
+        if ($('.datatitle').val() == false)
+        {
+            error_free = false;
+            error_message = "title not written";
+        }
+        if ($('.databody').val() == false)
+        {
+            error_free = false;
+            error_message = "body not written";
+        }
+        if (error_free)
+        {
+            $.ajax(options).done(function (data) {
+                var $target = $($form.attr("data-gt-target"));
+                $target.replaceWith(data);
+            });
+            return false;
+        }
+        else
+        {
+            alert("form incomplete "+ error_message);
+            return false;
+        }
+        
     }
-    $("form[data-zoc-ajax='true]").submit(ajaxFormSubmit);
+    $("form[data-gt-ajax=true]").submit(ajaxFormSubmit);
+    
 
 });
